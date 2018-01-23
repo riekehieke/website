9e678285684f70f546baef1f508f3b3aa4397413
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-01-23 18:01:17 +0100

    basic stuff

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+"use strict"
+// Look for .hamburger
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+});
