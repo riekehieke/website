972a54ebf39ad0ae900d1d422ff6a1e46f84ebea
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-01-27 22:48:16 +0100

    lightbox sternenhimmel

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,6 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("is-active");
   var menu = document.querySelector(".menu");
   menu.classList.toggle("active");
-  var overflow = document.querySelector(".body");
+  var overflow = document.querySelector("body");
   overflow.classList.toggle("overflow");
 });
