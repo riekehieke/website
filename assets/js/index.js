c2d84d3baae4f446f485d7a6dd56ba2e3bd99810
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-01-28 19:27:11 +0100

    jquery load - funktioniert noch nicht ganz

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,27 @@
 "use strict"
 $(document).ready(function () {
+  $(".content").load("assets/html/start.html");
+  console.log('start');
+});
+
+$(document).ready(function () {
   $("#load_start").on("click", function () {
-    $("#content").load("../html/start.html");
-    console.log('funktion läuft')
+    $(".content").load("assets/html/start.html");
+    console.log('projects')
+  });
+});
+
+$(document).ready(function () {
+  $("#load_about").on("click", function () {
+    $(".content").load("assets/html/about.html");
+    console.log('about')
+  });
+});
+
+$(document).ready(function () {
+  $("#load_cv").on("click", function () {
+    $(".content").load("assets/html/cv.html");
+    console.log('cv')
   });
 });
 
