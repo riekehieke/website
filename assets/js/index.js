87db06a0cb35d4b12bf6ef190a764f8cc15f4ab2
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-01-29 23:22:10 +0100

    scrolling in lightbox disabled

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,3 +8,33 @@ hamburger.addEventListener("click", function () {
   var overflow = document.querySelector("body");
   overflow.classList.toggle("overflow");
 });
+
+var img = document.querySelector(".img");
+img.addEventListener("click", function () {
+  var scrolling = document.querySelector("body");
+  scrolling.classList.toggle("overflow");
+});
+
+var img_last = document.querySelector(".img_last");
+img_last.addEventListener("click", function () {
+  var scrolling = document.querySelector("body");
+  scrolling.classList.toggle("overflow");
+});
+
+var hochformat = document.querySelector(".hochformat");
+hochformat.addEventListener("click", function () {
+  var scrolling = document.querySelector("body");
+  scrolling.classList.toggle("overflow");
+});
+
+var hochformat_last = document.querySelector(".hochformat_last");
+hochformat_last.addEventListener("click", function () {
+  var scrolling = document.querySelector("body");
+  scrolling.classList.toggle("overflow");
+});
+
+var lightbox = document.querySelector(".lightbox");
+lightbox.addEventListener("click", function () {
+  var scrolling = document.querySelector("body");
+  scrolling.classList.toggle("overflow");
+});
