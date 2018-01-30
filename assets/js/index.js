73cabd52658f309ad922cf3309886b47e591dfba
riekehieke <30436310+riekehieke@users.noreply.github.com>
2018-01-30 13:54:04 +0100

    navigation fixed (webapp ready)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,47 +1,157 @@
 "use strict"
+
+// on page load start.html
 $(document).ready(function () {
   $(".content").load("assets/html/start.html");
-  console.log('start');
 });
 
+// hamburger menu active
+$(document).ready(function () {
+  $(".hamburger").on("click", function () {
+    $(".hamburger").toggleClass("is-active");
+    console.log('hamburger active');
+    $(".menu").toggleClass("active");
+    console.log('menu active');
+    $(".body").toggleClass("overflow");
+  });
+});
+
+// menu click work
 $(document).ready(function () {
   $("#load_start").on("click", function () {
     $(".content").load("assets/html/start.html");
-    console.log('projects')
+    console.log('projects');
+    $(".hamburger").toggleClass("is-active");
+    console.log('hamburger active');
+    $(".menu").toggleClass("active");
+    console.log('menu active');
+    $(".body").toggleClass("overflow");
   });
 });
 
+// menu click about
 $(document).ready(function () {
   $("#load_about").on("click", function () {
     $(".content").load("assets/html/about.html");
-    console.log('about')
+    console.log('about');
+    $(".hamburger").toggleClass("is-active");
+    console.log('hamburger active');
+    $(".menu").toggleClass("active");
+    console.log('menu active');
+    $(".body").toggleClass("overflow");
   });
 });
 
+// menu click cv
 $(document).ready(function () {
   $("#load_cv").on("click", function () {
     $(".content").load("assets/html/cv.html");
-    console.log('cv')
+    console.log('cv');
+    $(".hamburger").toggleClass("is-active");
+    console.log('hamburger active');
+    $(".menu").toggleClass("active");
+    console.log('menu active');
+    $(".body").toggleClass("overflow");
   });
 });
 
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function () {
-  hamburger.classList.toggle("is-active");
-  var menu = document.querySelector(".menu");
-  menu.classList.toggle("active");
-  var overflow = document.querySelector("body");
-  overflow.classList.toggle("overflow");
+// menu click contact
+$(document).ready(function () {
+  $("#load_contact").on("click", function () {
+    $(".content").load("assets/html/contact.html");
+    console.log('cv');
+    $(".hamburger").toggleClass("is-active");
+    console.log('hamburger active');
+    $(".menu").toggleClass("active");
+    console.log('menu active');
+    $(".body").toggleClass("overflow");
+  });
 });
 
-var img = document.querySelector(".img");
-img.addEventListener("click", function () {
-  var scrolling = document.querySelector("body");
-  scrolling.classList.toggle("overflow");
+// menu click imprint
+$(document).ready(function () {
+  $("#load_imprint").on("click", function () {
+    $(".content").load("assets/html/imprint.html");
+    console.log('cv');
+    $(".hamburger").toggleClass("is-active");
+    console.log('hamburger active');
+    $(".menu").toggleClass("active");
+    console.log('menu active');
+    $(".body").toggleClass("overflow");
+  });
 });
 
-var lightbox = document.querySelector(".lightbox");
-lightbox.addEventListener("click", function () {
-  var body = document.querySelector("body");
-  body.classList.toggle("overflow");
+// on click load projects
+$(document).ready(function () {
+  $(".content").on("click", "#pr1", function () {
+    $(".content").load("assets/projects/time.html");
+  });
+  $(".content").on("click", "#pr2", function () {
+    $(".content").load("assets/projects/typro.html");
+  });
+  $(".content").on("click", "#pr3", function () {
+    $(".content").load("assets/projects/marketing.html");
+  });
+  $(".content").on("click", "#pr4", function () {
+    $(".content").load("assets/projects/bewerbung.html");
+  });
+  $(".content").on("click", "#pr5", function () {
+    $(".content").load("assets/projects/rh.html");
+  });
+  $(".content").on("click", "#pr6", function () {
+    $(".content").load("assets/projects/sternenhimmel.html");
+  });
+  $(".content").on("click", "#pr7", function () {
+    $(".content").load("assets/projects/circular.html");
+  });
+  $(".content").on("click", "#pr8", function () {
+    $(".content").load("assets/projects/abstract.html");
+  });
+  $(".content").on("click", "#pr9", function () {
+    $(".content").load("assets/projects/posters.html");
+  });
+  $(".content").on("click", "#pr10", function () {
+    $(".content").load("assets/projects/photography.html");
+  });
 });
+
+
+
+// lightbox overflow toggle
+$(document).ready(function () {
+  $(".content").on("click", ".img", function () {
+    $(".body").toggleClass("overflow");
+  });
+  $(".content").on("click", ".img_last", function () {
+    $(".body").toggleClass("overflow");
+  });
+  $(".content").on("click", ".hochformat", function () {
+    $(".body").toggleClass("overflow");
+  });
+  $(".content").on("click", ".hochformat_last", function () {
+    $(".body").toggleClass("overflow");
+  });
+  $(".content").on("click", ".lightbox", function () {
+    $(".body").toggleClass("overflow");
+  });
+});
+// var hamburger = document.querySelector(".hamburger");
+// hamburger.addEventListener("click", function () {
+//   hamburger.classList.toggle("is-active");
+//   var menu = document.querySelector(".menu");
+//   menu.classList.toggle("active");
+//   var overflow = document.querySelector("body");
+//   overflow.classList.toggle("overflow");
+// });
+
+// var img = document.querySelector(".img");
+// img.addEventListener("click", function () {
+//   var scrolling = document.querySelector("body");
+//   scrolling.classList.toggle("overflow");
+// });
+
+// var lightbox = document.querySelector(".lightbox");
+// lightbox.addEventListener("click", function () {
+//   var body = document.querySelector("body");
+//   body.classList.toggle("overflow");
+// });
