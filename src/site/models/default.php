6a3fc51f051d9d3dd238007dab60266c9b7ecba3
Jonas Kuske <mail@jonaskuske.com>
2022-07-27 02:18:02 +0200

    refactor: use shorthand content methods in model

--- src/site/models/default.php
+++ src/site/models/default.php
@@ -1,10 +1,8 @@
 <?php
 
-use Kirby\Cms\Field;
-use Kirby\Cms\Page;
-
 class DefaultPage extends Page {
   public function meta_description() {
-    return parent::content()->get('meta_description')->or(new Field(null, '', parent::text()->toBlocks()->excerpt()));
+    $excerpt = new Field(null, '', parent::text()->toBlocks()->excerpt());
+    return parent::meta_description()->or($excerpt);
   }
 }
--- src/site/models/project.php
+++ src/site/models/project.php
@@ -1,19 +1,17 @@
 <?php
 
-use Kirby\Cms\Field;
-use Kirby\Cms\Page;
-
 class ProjectPage extends Page {
   public function meta_description() {
-    return parent::content()->get('meta_description')->or(new Field(null, '', parent::text()->toBlocks()->excerpt()));
+    $excerpt = new Field(null, '', parent::text()->toBlocks()->excerpt());
+    return parent::meta_description()->or($excerpt);
   }
   public function meta_image() {
-    return parent::content()->get('meta_image')->or(parent::projectThumbnail());
+    return parent::meta_image()->or(parent::projectThumbnail());
   }
   public function og_image() {
-    return parent::content()->get('og_image')->or(parent::projectThumbnail());
+    return parent::og_image()->or(parent::projectThumbnail());
   }
   public function twitter_image() {
-    return parent::content()->get('twitter_image')->or(parent::projectThumbnail());
+    return parent::twitter_image()->or(parent::projectThumbnail());
   }
 }
